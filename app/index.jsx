a10a9a5ba88980feddb6cd1f92793f954b5b0446
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2024-09-26 11:30:23 -0300

    feat: create folders structure

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ import { Text, View } from "react-native";
 export default function Home() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">Aora!</Text>
+      <Text className="text-3xl font-pblack">Aora!</Text>
       <Link href="/profile" style={{ color: "blue" }}>
         Go to profile!
       </Link>
