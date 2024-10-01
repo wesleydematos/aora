e138977e2dbd51f615d2f4dac04f9cb554b34d3a
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2024-10-01 14:39:14 -0300

    feat: create signIn base compenent

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,24 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { Image, ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full h-full justify-center px-4 my-6">
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[115px] h-[35px]"
+          />
+          <Text className="text-2xl text-white mt-10 font-psemibold">
+            Log in to Aora
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
