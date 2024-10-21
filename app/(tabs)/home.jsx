b5e3bdbf16052bae8e88f7da13db27990177d924
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2024-10-21 11:20:03 -0300

    feat: added VideoCard and Trending to home tab

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,37 +1,47 @@
 import { useState } from "react";
-import { FlatList, Image, RefreshControl, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { FlatList, Image, RefreshControl, Text, View } from "react-native";
 
 import { images } from "../../constants";
-import SearchInput from "../../components/SearchInput";
-import EmptyState from "../../components/EmptyState";
+import useAppwrite from "../../lib/useAppwrite";
+import { getAllPosts, getLatestPosts } from "../../lib/appwrite";
+import { EmptyState, SearchInput, Trending, VideoCard } from "../../components";
 
 const Home = () => {
+  const { data: posts, refetch } = useAppwrite(getAllPosts);
+  const { data: latestPosts } = useAppwrite(getLatestPosts);
+
   const [refreshing, setRefreshing] = useState(false);
 
   const onRefresh = async () => {
     setRefreshing(true);
-    //refetch data;
+    await refetch();
     setRefreshing(false);
   };
 
   return (
     <SafeAreaView className="bg-primary">
       <FlatList
-        data={[]}
+        data={posts}
         keyExtractor={(item) => item.$id}
         renderItem={({ item }) => (
-          <Text className="text-3xl text-white">{item.id}</Text>
+          <VideoCard
+            title={item.title}
+            thumbnail={item.thumbnail}
+            video={item.video}
+            creator={item.creator.username}
+            avatar={item.creator.avatar}
+          />
         )}
         ListHeaderComponent={() => (
-          <View className="my-6 px-4 space-y-6">
-            <View className="justify-between items-start flex-row mb-6">
+          <View className="flex my-6 px-4 space-y-6">
+            <View className="flex justify-between items-start flex-row mb-6">
               <View>
-                <Text className="font-medium text-sm text-gray-100">
+                <Text className="font-pmedium text-sm text-gray-100">
                   Welcome Back
                 </Text>
                 <Text className="text-2xl font-psemibold text-white">
-                  Lara Silva
+                  Default User
                 </Text>
               </View>
 
@@ -50,6 +60,8 @@ const Home = () => {
               <Text className="text-lg font-pregular text-gray-100 mb-3">
                 Latest Videos
               </Text>
+
+              <Trending posts={latestPosts ?? []} />
             </View>
           </View>
         )}
