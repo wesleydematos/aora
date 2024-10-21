3f8730c12657026b248428079532c5732a65897c
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2024-10-21 11:18:49 -0300

    feat: create VideoCard

---
+++ components/index.js
@@ -0,0 +1,15 @@
+import FormField from "./FormField";
+import CustomButton from "./CustomButton";
+import Trending from "./Trending";
+import SearchInput from "./SearchInput";
+import EmptyState from "./EmptyState";
+import VideoCard from "./VideoCard";
+
+export {
+  FormField,
+  CustomButton,
+  Trending,
+  SearchInput,
+  EmptyState,
+  VideoCard,
+};
